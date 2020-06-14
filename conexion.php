875ac2a40bf4e04a842a3d80c75633807a1ff367
Luis Castro <ing.lcr.21@gmail.com>
2020-06-14 16:26:15 -0500

    Arreglos

--- conexion.php
+++ conexion.php
@@ -4,28 +4,28 @@
     $username = "root";
     $password = "";
     // Create connection
-    
+
     $conn = new mysqli($servername, $username, $password, $database);
 
     if ($conn->connect_errno) {
-    // La conexión falló. ¿Que vamos a hacer? 
-    // Se podría contactar con uno mismo (¿email?), registrar el error, mostrar una bonita página, etc.
-    // No se debe revelar información delicada
+        // La conexión falló. ¿Que vamos a hacer? 
+        // Se podría contactar con uno mismo (¿email?), registrar el error, mostrar una bonita página, etc.
+        // No se debe revelar información delicada
 
-    // Probemos esto:
-    echo "Lo sentimos, este sitio web está experimentando problemas.";
+        // Probemos esto:
+        echo "Lo sentimos, este sitio web está experimentando problemas.";
 
-    // Algo que no se debería de hacer en un sitio público, aunque este ejemplo lo mostrará
-    // de todas formas, es imprimir información relacionada con errores de MySQL -- se podría registrar
-    echo "Error: Fallo al conectarse a MySQL debido a: \n";
-    echo "Errno: " . $conn->connect_errno . "\n";
-    echo "Error: " . $conn->connect_error . "\n";
-    
-    // Podría ser conveniente mostrar algo interesante, aunque nosotros simplemente saldremos
-    exit;
-}else{
-    //echo "bien";
-}
+        // Algo que no se debería de hacer en un sitio público, aunque este ejemplo lo mostrará
+        // de todas formas, es imprimir información relacionada con errores de MySQL -- se podría registrar
+        echo "Error: Fallo al conectarse a MySQL debido a: \n";
+        echo "Errno: " . $conn->connect_errno . "\n";
+        echo "Error: " . $conn->connect_error . "\n";
+
+        // Podría ser conveniente mostrar algo interesante, aunque nosotros simplemente saldremos
+        exit;
+    } else {
+        //echo "bien";
+    }
     ?>
     
     
--- index.php
+++ index.php
@@ -1,34 +1,77 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Leer Archivo Excel usando PHP</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-	<h2>Leer Archivos Excel</h2>	
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <h3 class="panel-title">Resultados de archivo de Excel.</h3>
-      </div>
-      <div class="panel-body">
-        <div class="col-lg-12">
-            
 
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-5">
+
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Importar archivo de excel del mes actual.</h3>
 
             <form method="POST" action="importar.php" enctype="multipart/form-data">
-              <label>Importar:</label> 
-              <input type="file" name="uploadedFile" ><br>
-
-              <input type="submit" name="enviar" value="Enviar" class="btn btn-primary">
+              <label>Archivo de excel:</label>
+              <input type="file" name="uploadedFile" required>
+              <br>
+              <input type="submit" name="enviar" value="Procesar" class="btn btn-primary mt-3">
             </form>
 
+          </div>
+        </div>
 
-        </div>	
-      </div>	
       </div>
+      <div class="col-12 col-md-7">
+
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Planes de las líneas.</h3>
+
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Línea</th>
+                  <th scope="col">Plan</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+
+                require 'conexion.php';
+
+                $sql = "SELECT * FROM celulares";
+                $result = $conn->query($sql);
+
+                while ($row = $result->fetch_assoc()) {
+
+                ?>
+
+                  <tr>
+                    <td><?php echo $row["celular"] ?></td>
+                    <td>$<?php echo number_format($row["total_plan"]) ?></td>
+                  </tr>
+
+                <?php } ?>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+
 </body>
+
 </html>
